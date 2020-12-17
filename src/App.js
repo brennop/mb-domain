fb703c5cfd9aefb1b175981554d251dad6cbed99
brennop <brennopcordeiro@gmail.com>
2020-12-17 20:29:44 -0300

    remove service worker for real

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        O App do Mandou Bem mudou de casa.
+        <a href="https://mandoubem.cjr.org.br">mandoubem.cjr.org.br</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
